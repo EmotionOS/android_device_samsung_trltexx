55dbc05f6595dba129aaa5a39668cfb5d5ed1ee1
crimsonthunder <crimsonthunder54@gmail.com>
2016-09-11 22:49:16 +0200

    SaberMod

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,3 +41,69 @@ TARGET_UNIFIED_DEVICE := true
 
 # inherit from the proprietary version
 -include vendor/samsung/trltexx/BoardConfigVendor.mk
+
+# Disable dex-preoptimization
+WITH_DEXPREOPT := false
+
+  # Custom Toolchain
+  TARGET_NDK_VERSION := 5.3
+  TARGET_SM_AND := 5.3
+  TARGET_SM_KERNEL := 7.0
+  DRAGONTC_VERSION := 3.9
+
+  # CMRemix-MM Optimization
+  GRAPHITE_OPTIMIZATION := false
+  FLOOP_NEST_OPTIMIZE := false
+  LOCAL_STRICT_ALIASING := false
+  ENABLE_GCC_DEFAULTS := false
+  O3_OPTIMIZATIONS := false
+  DISABLE_DTC_OPTS := false
+  ENABLE_PTHREAD := false
+  GCC_ONLY_OPTIMIZATION := false
+  ENABLE_DTC_LTO := false
+  USE_ARM_MODE := false
+  IPA_OPTIMIZATIONS := false
+  OPENMP_OPTIMIZATIONS := false
+  MEMORY_LEAK_OPTIMIZATIONS := false
+  ARCHIDROID_OPTIMIZATIONS := false
+
+# No Optimization Bluetooth modules
+LOCAL_BLUETOOTH_BLUEDROID := \
+  bluetooth.default \
+  libbt-brcm_stack \
+  audio.a2dp.default \
+  libbt-brcm_gki \
+  libbt-utils \
+  libbt-qcom_sbc_decoder \
+  libbt-brcm_bta \
+  bdt \
+  bdtest \
+  libbt-hci \
+  libosi \
+  ositests \
+  libbt-vendor \
+  libbluetooth_jni
+  
+ifndef NO_OPTIMIZATIONS
+  NO_OPTIMIZATIONS := $(LOCAL_BLUETOOTH_BLUEDROID) libadbd
+else
+  NO_OPTIMIZATIONS += $(LOCAL_BLUETOOTH_BLUEDROID) libadbd
+endif
+
+# Dalvik/Art
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.sys.fw.dex2oat_thread_count=4
+
+    # DragonTC info
+DRAGONTC_VERSION := $(DRAGONTC_VERSION)
+export $(DRAGONTC_VERSION)
+
+DTC_PATH := prebuilts/clang/linux-x86/host/$(DRAGONTC_VERSION)/
+DTC_VER := $(shell cat $(DTC_PATH)/VERSION)
+export $(DTC_VER)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.dtc.version=$(DTC_VER)
+
+# Inherite sabermod Config
+-include vendor/emotion/config/sm_board.mk
